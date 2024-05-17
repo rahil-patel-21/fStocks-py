b02d6f30df1b04def33f498a6a143a4a9d67e82a
TechAddict <rahil.311298@gmail.com>
2024-05-17 22:21:41 +0530

    Cron added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Imports
 import os
+import time
 import json
+import requests # type: ignore
+import schedule # type: ignore
 from fastapi import FastAPI # type: ignore
 from dhanhq import marketfeed # type: ignore
 from dotenv import load_dotenv # type: ignore
@@ -17,6 +20,7 @@ async def on_connect(instance):
 file_path = "store/nifty_50.txt"
 async def on_message(instance, message):
     try:
+        print(message)
         if (message['type'] == 'Ticker Data'):
             file = open(file_path, "a")
             json_data = json.dumps(message)
@@ -33,9 +37,36 @@ feed = marketfeed.DhanFeed(DHAN_CLIENT_CODE,
     on_connect=on_connect,
     on_message=on_message)
 
-app = FastAPI()
+feed.run_forever()
 
-@app.get('/init')
-def init():
-    feed.run_forever()
-    return {'code_version': CODE_VERSION}
+# app = FastAPI()
+
+# @app.get('/init')
+# def init():
+#     feed.run_forever()
+#     return {'code_version': CODE_VERSION}
+
+# def cronJob():
+#     # Define the URL and payload data
+#     url = 'https://scanx.dhan.co/scanx/optchain'
+#     payload = {
+#         "Data": {
+#             "Seg": 0,
+#             "Sid": 13,
+#             "Exp": 1400265000
+#         }
+#     }
+#     headers = {'Content-Type': 'text/plain'}
+
+#     # Send a POST request with the payload data
+#     response = requests.post(url, json=payload, headers=headers)
+
+#     # Print the response content
+#     print(response.text)
+
+# # Scheduling the job to run every 5 seconds
+# schedule.every(5).seconds.do(cronJob)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
